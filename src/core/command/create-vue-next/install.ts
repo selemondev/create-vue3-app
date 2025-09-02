60016a953c60c8767a10d2dd3d4e87252e92ca6a
selemondev <selemondev19@gmail.com>
2025-09-02 12:57:53 -0600

    feat: check dependency updates

--- src/core/command/create-vue-next/install.ts
+++ src/core/command/create-vue-next/install.ts
@@ -1,91 +1,104 @@
-import options from '../../../core/utils/vue/options'
-import { logger } from '../../../utils/logger'
-import { createSpawnCmd } from '../../../utils/createSpawnCmd'
-import ora from 'ora';
-import chalk from "chalk"
+import options from "../../../core/utils/vue/options";
+import { logger } from "../../../utils/logger";
+import { createSpawnCmd } from "../../../utils/createSpawnCmd";
+import ora from "ora";
+import chalk from "chalk";
+import { packageManagerExecutable } from "../../../utils/package-manager-executable";
 async function installDeps() {
-
   // No output will be shown in the console
-  const cmdIgnore = createSpawnCmd(options.dest, 'ignore')
-
-  const spinner = ora('Copying template...').start();
+  const cmdIgnore = createSpawnCmd(options.dest, "ignore");
+  const spinner = ora();
 
   const startTime: number = new Date().getTime();
 
   if (options.useGitInit) {
-    await cmdIgnore('git', ['init'])
+    await cmdIgnore("git", ["init"]);
 
-    await cmdIgnore('git', ['add .'])
+    await cmdIgnore("git", ["add ."]);
 
-    await cmdIgnore('git', ['commit -m "Initialized by create-vue3-app"'])
+    await cmdIgnore("git", ['commit -m "Initialized by create-vue3-app"']);
   }
 
-
-  if (options.package && options.package !== 'none') {
-    spinner.text = chalk.cyan(`Installing dependencies with ${options.package}. Please wait...`);
-
-    await cmdIgnore(options.package, ['install']);
+  if (options.package && options.package !== "none") {
+    spinner.start(`Checking for dependency updates with ${options.package}.`);
+    await cmdIgnore(packageManagerExecutable(options.package), [
+      "npm-check-updates",
+    ]);
+    spinner.text = chalk.green(
+      `Checking for dependency updates with ${options.package}.`,
+    );
+    spinner.succeed();
+    spinner.start(`Updating dependencies.`);
+    await cmdIgnore(packageManagerExecutable(options.package), [
+      "npm-check-updates -u",
+    ]);
+    spinner.text = chalk.green(`Updating dependencies.`);
+    spinner.succeed();
+    spinner.start(`Installing the latest dependencies.`);
+    await cmdIgnore(options.package, ["install"]);
+    spinner.text = chalk.green(`Installing the latest dependencies.`);
+    spinner.succeed();
   }
 
-  spinner.stop();
+  const endTime: number = new Date().getTime();
+  const usageTime: number = (endTime - startTime) / 1000;
 
-  const endTime: number = new Date().getTime()
-  const usageTime: number = (endTime - startTime) / 1000
+  console.log();
 
-  console.log()
+  logger.info(`🚀 Completed in ${usageTime}s`);
 
-  logger.info(
-    `🚀 Completed in ${usageTime}s`
-  );
+  console.log();
 
-  console.log()
+  logger.success("✅ Project created successfully");
 
-  logger.success('✅ Project created successfully')
+  console.log();
 
-  console.log()
+  logger.info(`cd ${options.name}`);
 
-  logger.info(`cd ${options.name}`)
+  console.log();
 
-  console.log()
-
-  if (options.package !== 'none') {
+  if (options.package !== "none") {
     logger.info(
-      options.package === 'npm'
+      options.package === "npm"
         ? `${options.package} run dev to start the dev server`
         : `${options.package} dev to start the dev server`,
-    )
+    );
 
-    console.log()
+    console.log();
 
-    options.useEslint && logger.info(options.package === 'npm'
-      ? `${options.package} run lint`
-      : `${options.package} lint`)
+    options.useEslint &&
+      logger.info(
+        options.package === "npm"
+          ? `${options.package} run lint`
+          : `${options.package} lint`,
+      );
 
-    options.useEslint && console.log()
+    options.useEslint && console.log();
 
-    options.useVitest && logger.info(`${options.package} run test:unit to run tests`);
-  
-    options.useVitest && console.log()
+    options.useVitest &&
+      logger.info(`${options.package} run test:unit to run tests`);
+
+    options.useVitest && console.log();
 
     options.useTypeScript && logger.info(`${options.package} run type-check`);
-
-
   } else {
-    logger.info(`npm install - To install dependencies`)
+    logger.info(`npm install - To install dependencies`);
 
-    console.log()
+    console.log();
 
-    options.useEslint && logger.info('npm run lint to format your code')
+    options.useEslint && logger.info("npm run lint to format your code");
 
-    options.useEslint && console.log()
+    options.useEslint && console.log();
 
-    logger.info('npm run dev to start the dev server')
+    logger.info("npm run dev to start the dev server");
 
-    options.useVitest && console.log()
+    options.useVitest && console.log();
 
-    options.useVitest && logger.info('npm run test:unit to run tests')
+    options.useVitest && logger.info("npm run test:unit to run tests");
 
-    options.useTypeScript && logger.info("npm run type-check")
+    options.useTypeScript && console.log();
+
+    options.useTypeScript && logger.info("npm run type-check");
   }
 }
-export default installDeps
+export default installDeps;
--- src/core/questions/vue/prompts.ts
+++ src/core/questions/vue/prompts.ts
@@ -1,55 +1,56 @@
 const prompt = [
-    {
-        name: 'useRouter',
-        type: () => 'toggle',
-        message: 'Add Vue Router for Single Page Application development?',
-        initial: true,
-        active: 'Yes',
-        inactive: 'No'
-    },
-    {
-        name: 'usePinia',
-        type: () => 'toggle',
-        message: 'Add Pinia for state management?',
-        initial: true,
-        active: 'Yes',
-        inactive: 'No'
-    },
+  {
+    name: "useRouter",
+    type: () => "toggle",
+    message: "Add Vue Router for Single Page Application development?",
+    initial: true,
+    active: "Yes",
+    inactive: "No",
+  },
+  {
+    name: "usePinia",
+    type: () => "toggle",
+    message: "Add Pinia for state management?",
+    initial: true,
+    active: "Yes",
+    inactive: "No",
+  },
 
-    {
-        name: 'useVueUse',
-        type: () => 'toggle',
-        message: 'Add VueUse for a collection of essential Vue composition utilities?',
-        initial: false,
-        active: 'Yes',
-        inactive: 'No'
-    },
+  {
+    name: "useVueUse",
+    type: () => "toggle",
+    message:
+      "Add VueUse for a collection of essential Vue composition utilities?",
+    initial: false,
+    active: "Yes",
+    inactive: "No",
+  },
 
-    {
-        name: 'useTanStackVueQuery',
-        type: () => 'toggle',
-        message: 'Add TanStack Vue Query for server state management?',
-        initial: true,
-        active: 'Yes',
-        inactive: 'No'
-    },
+  {
+    name: "useTanStackVueQuery",
+    type: () => "toggle",
+    message: "Add TanStack Vue Query for server state management?",
+    initial: true,
+    active: "Yes",
+    inactive: "No",
+  },
 
-    {
-        name: 'useDevTool',
-        type: () => 'toggle',
-        message: 'Add Vue DevTools to enhance the development experience? (experimental)',
-        initial: true,
-        active: 'Yes',
-        inactive: 'No'
-    },
+  {
+    name: "useDevTool",
+    type: () => "toggle",
+    message: "Add Vue DevTools to enhance the development experience?",
+    initial: true,
+    active: "Yes",
+    inactive: "No",
+  },
 
-    {
-        name: 'useVitest',
-        type: () => 'toggle',
-        message: 'Add Vitest for unit testing?',
-        initial: true,
-        active: 'Yes',
-        inactive: 'No'
-    }
-]
-export default prompt
+  {
+    name: "useVitest",
+    type: () => "toggle",
+    message: "Add Vitest for unit testing?",
+    initial: true,
+    active: "Yes",
+    inactive: "No",
+  },
+];
+export default prompt;
